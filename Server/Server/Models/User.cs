36ffe1fc0bf668017dc4e13f767bd2f05ec492b4
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-05-22 19:30:03 +0100

    created UserModel

--- Server/Server/Models/User.cs
+++ Server/Server/Models/User.cs
@@ -7,5 +7,8 @@ namespace Server.Models
 {
     public class User
     {
+        public string Id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
     }
 }
