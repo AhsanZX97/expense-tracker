4d5faa74edff960cb0e64cb100418048006b4ee2
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-05-22 19:56:35 +0100

    added some rules for the models

--- Server/Server/Models/User.cs
+++ Server/Server/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,16 @@ namespace Server.Models
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
+        [Required]
         public string username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+        [Range(1, 16)]
+        [Required]
         public string password { get; set; }
     }
 }
