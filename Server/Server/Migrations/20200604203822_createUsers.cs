4ddc5218ab01f61d0437b2bf0ca4b822ae456e14
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-06-04 21:43:32 +0100

    added migrations

---
+++ Server/Server/Migrations/20200604203822_createUsers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Server.Migrations
+{
+    public partial class createUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    username = table.Column<string>(nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserItems");
+        }
+    }
+}
